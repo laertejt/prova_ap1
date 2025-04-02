2d33b275ff648b7ae30d6612eadf835ceb78a2ff
Laerte Jun Takeuti <laertejt@gmail.com>
2025-04-01 21:31:12 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 03-abril-2025 - PROVA AP1
+
+## Programação para Análise de Dados
+
+### Prof. Laerte Jun Takeuti, Msc
---
+++ prova.py
@@ -0,0 +1,15 @@
+# 1 - (1,0 ponto) Leia o arquivo imoveis_brasil
+# 2 - (1,0 ponto) Encontre a cidade com o imóvel mais caro da amostra.
+# 3 - (1,0 ponto) Descubra qual é o ano da contrução do imovel mais barato de Curitiba.
+# 4 - (1,0 ponto) Leia o arquivo olympics_historico.
+# 5 - (1,0 ponto) Calcule a media salarial do Homem e a média salarial da Mulher.
+# 6 - (1,0 ponto) Encontre o nome e o email da pessoa mais nova da Area de formação de "Direito".
+# 7 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/planilhao/
+# 8 - (1,0 ponto) Encontre a empresa com o valor do ROC mais alto do dia "2025-04-01"
+# 9 - (1,0 ponto) Leia o arquivo ibov.csv
+# 10 - (1,0 ponto) Faça o grafico do preço de fechmanero do ibovespa x data.
+# 11 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/balanco/
+# 12 - (1,0 ponto) Calcule o ROE da empresa BBAS3 do trimestre 4o.trimestre 2024 (20244T)
+# ROE = (3.11 - Lucro ou Prejuízo Líquido Consolidado do Período) / (2.07 - Patrimônio Líquido Consolidado)
+# 13 - (1,0 ponto) Salve sua resoluçao dos exercícios e suba no seu github com o nome "seunome_ap1" 
+
