ecc5dd455fb1d7e8d775c1b7617746a440bdd4b1
Laerte Jun Takeuti <laertejt@gmail.com>
2025-04-02 07:30:46 -0300

    initial commit

--- prova.py
+++ prova.py
@@ -1,22 +1,22 @@
-# 1 - (1,0 ponto) Leia o arquivo imoveis_brasil
+# 1 - (0,5 ponto) Leia o arquivo imoveis_brasil
 # 2 - (1,0 ponto) Encontre a cidade com o imóvel mais caro da amostra.
 # 3 - (1,0 ponto) Descubra qual é o ano da contrução do imovel mais barato de Curitiba.
-# 4 - (1,0 ponto) Leia o arquivo olympics_historico.
+# 4 - (0,5 ponto) Leia o arquivo olympics_historico.
 # 5 - (1,0 ponto) Calcule a media salarial do Homem e a média salarial da Mulher.
 # 6 - (1,0 ponto) Encontre o nome e o email da pessoa mais nova da Area de formação de "Direito".
-# 7 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/planilhao/ para o dia "2025-04-01":
+# 7 - (0,5 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/planilhao/ para o dia "2025-04-01":
         # params = {
         # 'data_base': '2024-04-01'
         # }
 # 8 - (1,0 ponto) Encontre a empresa com o valor do ROC mais alto.
-# 9 - (1,0 ponto) Leia o arquivo ibov.csv
-# 10 - (1,0 ponto) Faça o grafico do preço de fechmanero do ibovespa x data.
-# 11 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/balanco/ da empresa Banco do Brasil do trimestre 4o.trimestre 2024.
+# 9 - (0,5 ponto) Leia o arquivo ibov.csv
+# 10 - (1,0 ponto) Faça o grafico do preço de fechmanero x data do ibovespa.
+# 11 - (0,5 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/balanco/ da empresa Banco do Brasil do trimestre 4o.trimestre 2024.
         # params = {
         #     'ticker': 'BBAS3',
         #     'ano_tri': '20244T',
         # }
 # 12 - (1,0 ponto) Calcule o ROE do banco.
         # ROE = (3.11 - Lucro ou Prejuízo Líquido Consolidado do Período) / (2.07 - Patrimônio Líquido Consolidado)
-# 13 - (1,0 ponto) Salve sua resoluçao dos exercícios e suba no seu github com o nome "seunome_ap1" 
+# 13 - (0,5 ponto) Salve sua resoluçao dos exercícios e suba no seu github com o nome "seunome_ap1" 
 
