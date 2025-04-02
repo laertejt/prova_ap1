e2e9402726c273117797236e856e277e2079bd3a
Laerte Jun Takeuti <laertejt@gmail.com>
2025-04-01 21:37:50 -0300

    initial commit

--- prova.py
+++ prova.py
@@ -4,12 +4,19 @@
 # 4 - (1,0 ponto) Leia o arquivo olympics_historico.
 # 5 - (1,0 ponto) Calcule a media salarial do Homem e a média salarial da Mulher.
 # 6 - (1,0 ponto) Encontre o nome e o email da pessoa mais nova da Area de formação de "Direito".
-# 7 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/planilhao/
-# 8 - (1,0 ponto) Encontre a empresa com o valor do ROC mais alto do dia "2025-04-01"
+# 7 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/planilhao/ para o dia "2025-04-01":
+        # params = {
+        # 'data_base': '2024-04-01'
+        # }
+# 8 - (1,0 ponto) Encontre a empresa com o valor do ROC mais alto.
 # 9 - (1,0 ponto) Leia o arquivo ibov.csv
 # 10 - (1,0 ponto) Faça o grafico do preço de fechmanero do ibovespa x data.
 # 11 - (1,0 ponto) Acesse a api: https://laboratoriodefinancas.com/api/v1/balanco/
-# 12 - (1,0 ponto) Calcule o ROE da empresa BBAS3 do trimestre 4o.trimestre 2024 (20244T)
-# ROE = (3.11 - Lucro ou Prejuízo Líquido Consolidado do Período) / (2.07 - Patrimônio Líquido Consolidado)
+        # params = {
+        #     'ticker': 'BBAS3',
+        #     'ano_tri': '20244T',
+        # }
+# 12 - (1,0 ponto) Calcule o ROE da empresa Banco do Brasil do trimestre 4o.trimestre 2024
+        # ROE = (3.11 - Lucro ou Prejuízo Líquido Consolidado do Período) / (2.07 - Patrimônio Líquido Consolidado)
 # 13 - (1,0 ponto) Salve sua resoluçao dos exercícios e suba no seu github com o nome "seunome_ap1" 
 
